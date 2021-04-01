cc1b8cd39ff44b381e427e8a4fe0b0949e6dca5a
BuildTools <unconfigured@null.spigotmc.org>
2021-04-01 15:12:41 -0300

    Atv3

---
+++ Atv3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Atv3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> valores = new List<int>();
+            int ciclos, count = 0;
+            ciclos = 10;
+
+            while (count < ciclos)
+            {
+                valores.Add(Convert.ToInt32(Console.ReadLine()));
+                count++;
+            }
+
+            List<int> limpo = new List<int>(), contagem = new List<int>();
+            valores.Sort();
+            foreach(int val in valores)
+            {
+                if(limpo.IndexOf(val) != -1)
+                {
+                    contagem[limpo.IndexOf(val)]++;
+                }
+                else
+                {
+                    limpo.Add(val);
+                    contagem.Add(1);
+                }
+            }
+            int quantidade;
+            string plural;
+            foreach(int val in limpo)
+            {
+                quantidade = contagem[limpo.IndexOf(val)];
+                plural = (quantidade > 1) ? "es." : ".";
+                Console.WriteLine("O valor " + val + " apareceu " + quantidade + " vez" + plural);
+            }
+        }
+    }
+}
