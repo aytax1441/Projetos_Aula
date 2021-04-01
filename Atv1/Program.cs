29300b9e8ba54531e89747ee99baa21c1fc57090
BuildTools <unconfigured@null.spigotmc.org>
2021-04-01 14:14:12 -0300

    Adicionar arquivos de projeto.

---
+++ Atv1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Atv1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] valores = new int[3];
+            int? maior;
+            int count = 0, ciclos;
+            maior = null;
+            ciclos = 3;
+            while(count < ciclos)
+            {
+                valores[count] = Console.Read();
+                if(maior == null)
+                {
+                    maior = valores[count];
+                }
+                else
+                {
+                    if(valores[count] > maior)
+                    {
+                        maior = valores[count];
+                    }
+                }
+            }
+
+        }
+    }
+}
